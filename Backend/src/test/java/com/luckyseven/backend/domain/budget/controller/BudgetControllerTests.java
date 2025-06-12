043c15a1ef42c2004461c26736460e4b689e1949
mipangg <morganjeong119@gmail.com>
2025-06-12 10:06:44 +0900

    test(Budget): BudgetService 테스트 수정

--- Backend/src/test/java/com/luckyseven/backend/domain/budget/controller/BudgetControllerTests.java
+++ Backend/src/test/java/com/luckyseven/backend/domain/budget/controller/BudgetControllerTests.java
@@ -4,6 +4,7 @@ import static com.luckyseven.backend.domain.budget.util.TestUtils.genAuthenticat
 import static com.luckyseven.backend.domain.budget.util.TestUtils.genBudgetAddReq;
 import static com.luckyseven.backend.domain.budget.util.TestUtils.genBudgetCreateReq;
 import static com.luckyseven.backend.domain.budget.util.TestUtils.genBudgetCreateResp;
+import static com.luckyseven.backend.domain.budget.util.TestUtils.genBudgetReadResp;
 import static com.luckyseven.backend.domain.budget.util.TestUtils.genBudgetUpdateRespAfterAdd;
 import static com.luckyseven.backend.domain.budget.util.TestUtils.genMemberDetails;
 import static com.luckyseven.backend.domain.budget.util.TestUtils.genBudgetUpdateReq;
@@ -49,236 +50,227 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc(addFilters = false)
 class BudgetControllerTests {
 
-    @Autowired
-    private MockMvc mockMvc;
+  @Autowired
+  private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+  @Autowired
+  private ObjectMapper objectMapper;
 
-    @MockitoBean
-    private BudgetService budgetService;
+  @MockitoBean
+  private BudgetService budgetService;
 
-    @MockitoBean
-    private BudgetValidator budgetValidator;
+  @MockitoBean
+  private BudgetValidator budgetValidator;
 
-    // SecurityContext 정리
-    @AfterEach
-    void tearDown() {
-        SecurityContextHolder.clearContext();
-    }
+  // SecurityContext 정리
+  @AfterEach
+  void tearDown() {
+    SecurityContextHolder.clearContext();
+  }
 
-    @Test
-    @DisplayName("POST /api/teams/{teamId}/budgets로 요청해 예산 생성이 성공하면 201 CREATED를 응답한다")
-    void create_should_return_201() throws Exception {
+  @Test
+  @DisplayName("POST /api/teams/{teamId}/budgets로 요청해 예산 생성이 성공하면 201 CREATED를 응답한다")
+  void create_should_return_201() throws Exception {
 
-        MemberDetails member = genMemberDetails();
-        genAuthentication(member);
+    MemberDetails member = genMemberDetails();
+    genAuthentication(member);
 
-        Long teamId = 1L;
-        BudgetCreateRequest req = genBudgetCreateReq();
-        BudgetCreateResponse resp = genBudgetCreateResp();
+    Long teamId = 1L;
+    BudgetCreateRequest req = genBudgetCreateReq();
+    BudgetCreateResponse resp = genBudgetCreateResp();
 
-        when(budgetService.save(teamId, member.getId(), req)).thenReturn(resp);
+    when(budgetService.save(teamId, member.getId(), req)).thenReturn(resp);
 
-        mockMvc.perform(
-                        post("/api/teams/{teamId}/budgets", teamId)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(req)))
-                .andExpect(status().isCreated())
-                .andDo(print());
+    mockMvc.perform(
+            post("/api/teams/{teamId}/budgets", teamId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(req)))
+        .andExpect(status().isCreated())
+        .andDo(print());
 
-        verify(budgetValidator, times(1)).validateRequest(req);
-    }
+    verify(budgetValidator, times(1)).validateRequest(req);
+  }
 
-    @Test
-    @DisplayName("totalAmount가 null인 요청을 받으면 400 BAD REQUEST와 함께 예산 생성에 실패한다")
-    void create_should_return_400() throws Exception {
+  @Test
+  @DisplayName("totalAmount가 null인 요청을 받으면 400 BAD REQUEST와 함께 예산 생성에 실패한다")
+  void create_should_return_400() throws Exception {
 
-        MemberDetails member = genMemberDetails();
-        genAuthentication(member);
+    MemberDetails member = genMemberDetails();
+    genAuthentication(member);
 
-        Long teamId = 1L;
-        BudgetCreateRequest req = new BudgetCreateRequest(
-                null,
-                true,
-                BigDecimal.valueOf(1393.7),
-                CurrencyCode.USD
-        );
+    Long teamId = 1L;
+    BudgetCreateRequest req = new BudgetCreateRequest(
+        null,
+        true,
+        BigDecimal.valueOf(1393.7),
+        CurrencyCode.USD
+    );
 
-        when(budgetService.save(teamId, member.getId(), req))
-                .thenThrow(new CustomLogicException(ExceptionCode.BAD_REQUEST));
+    when(budgetService.save(teamId, member.getId(), req))
+        .thenThrow(new CustomLogicException(ExceptionCode.BAD_REQUEST));
 
-        mockMvc.perform(
-                        post("/api/teams/{teamId}/budgets", teamId)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(req))
-                )
-                .andExpect(status().isBadRequest())
-                .andDo(print());
+    mockMvc.perform(
+            post("/api/teams/{teamId}/budgets", teamId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(req))
+        )
+        .andExpect(status().isBadRequest())
+        .andDo(print());
 
-    }
+  }
 
-    @Test
-    @DisplayName("GET /api/teams/{teamId}/budgets 을 요청해 팀 예산 조회에 성공하면 200 OK로 응답한다")
-    void read_should_return_200() throws Exception {
+  @Test
+  @DisplayName("GET /api/teams/{teamId}/budgets 을 요청해 팀 예산 조회에 성공하면 200 OK로 응답한다")
+  void read_should_return_200() throws Exception {
 
-        Long teamId = 1L;
-        BudgetReadResponse resp = new BudgetReadResponse(
-                1L,
-                LocalDateTime.now(),
-                1L,
-                BigDecimal.valueOf(100000),
-                BigDecimal.valueOf(100000),
-                CurrencyCode.USD,
-                null,
-                null
-        );
+    Long teamId = 1L;
+    BudgetReadResponse resp = genBudgetReadResp();
 
-        when(budgetService.getByTeamId(teamId)).thenReturn(resp);
+    when(budgetService.getByTeamId(teamId)).thenReturn(resp);
 
-        mockMvc.perform(
-                        get("/api/teams/{teamId}/budgets", teamId)
-                )
-                .andExpect(status().isOk())
-                .andDo(print());
-    }
+    mockMvc.perform(
+            get("/api/teams/{teamId}/budgets", teamId)
+        )
+        .andExpect(status().isOk())
+        .andDo(print());
+  }
 
-    @Test
-    @DisplayName("존재하지 않는 teamId로 예산 조회를 할 경우 404 NOT FOUND가 발생한다")
-    void read_should_return_404() throws Exception {
+  @Test
+  @DisplayName("존재하지 않는 teamId로 예산 조회를 할 경우 404 NOT FOUND가 발생한다")
+  void read_should_return_404() throws Exception {
 
-        Long teamId = 1L;
-        when(budgetService.getByTeamId(teamId))
-                .thenThrow(new CustomLogicException(ExceptionCode.TEAM_NOT_FOUND));
+    Long teamId = 1L;
+    when(budgetService.getByTeamId(teamId))
+        .thenThrow(new CustomLogicException(ExceptionCode.TEAM_NOT_FOUND));
 
-        mockMvc.perform(
-                        get("/api/teams/{teamId}/budgets", teamId)
-                )
-                .andExpect(status().isNotFound())
-                .andDo(print());
+    mockMvc.perform(
+            get("/api/teams/{teamId}/budgets", teamId)
+        )
+        .andExpect(status().isNotFound())
+        .andDo(print());
 
-    }
+  }
 
-    @Test
-    @DisplayName("DELETE /api/teams/{teamId}/budgets 를 요청해 예산 삭제에 성공하면 204 NO CONTENT를 응답한다")
-    void delete_should_return_204() throws Exception {
+  @Test
+  @DisplayName("DELETE /api/teams/{teamId}/budgets 를 요청해 예산 삭제에 성공하면 204 NO CONTENT를 응답한다")
+  void delete_should_return_204() throws Exception {
 
-        Long teamId = 1L;
+    Long teamId = 1L;
 
-        mockMvc.perform(
-                        delete("/api/teams/{teamId}/budgets", teamId)
-                )
-                .andExpect(status().isNoContent())
-                .andDo(print());
+    mockMvc.perform(
+            delete("/api/teams/{teamId}/budgets", teamId)
+        )
+        .andExpect(status().isNoContent())
+        .andDo(print());
 
-        verify(budgetService, times(1)).deleteByTeamId(teamId);
+    verify(budgetService, times(1)).deleteByTeamId(teamId);
 
-    }
+  }
 
-    @Test
-    @DisplayName("PATCH /api/teams/{teamId}/budgets를 요청해 예산 수정에 성공하면 200 OK를 응답한다")
-    void update_should_return_200() throws Exception {
+  @Test
+  @DisplayName("PATCH /api/teams/{teamId}/budgets를 요청해 예산 수정에 성공하면 200 OK를 응답한다")
+  void update_should_return_200() throws Exception {
 
-        Long teamId = 1L;
+    Long teamId = 1L;
 
-        MemberDetails member = genMemberDetails();
-        genAuthentication(member);
+    MemberDetails member = genMemberDetails();
+    genAuthentication(member);
 
-        BudgetUpdateRequest req = genBudgetUpdateReq();
-        BudgetUpdateResponse resp = genBudgetUpdateResp();
+    BudgetUpdateRequest req = genBudgetUpdateReq();
+    BudgetUpdateResponse resp = genBudgetUpdateResp();
 
-        when(budgetService.updateByTeamId(teamId, member.getId(), req)).thenReturn(resp);
+    when(budgetService.updateByTeamId(teamId, member.getId(), req)).thenReturn(resp);
 
-        mockMvc.perform(
-                        patch("/api/teams/{teamId}/budgets", teamId)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(req))
-                )
-                .andExpect(status().isOk())
-                .andDo(print());
+    mockMvc.perform(
+            patch("/api/teams/{teamId}/budgets", teamId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(req))
+        )
+        .andExpect(status().isOk())
+        .andDo(print());
 
-        verify(budgetValidator, times(1)).validateRequest(req);
-    }
+    verify(budgetValidator, times(1)).validateRequest(req);
+  }
 
-    @Test
-    @DisplayName("totalAmount를 null로 요청한 경우 예산 수정에 실패하고 400 BAD REQUEST를 응답한다")
-    void update_should_return_400() throws Exception {
+  @Test
+  @DisplayName("totalAmount를 null로 요청한 경우 예산 수정에 실패하고 400 BAD REQUEST를 응답한다")
+  void update_should_return_400() throws Exception {
 
-        Long teamId = 1L;
+    Long teamId = 1L;
 
-        MemberDetails member = genMemberDetails();
-        genAuthentication(member);
+    MemberDetails member = genMemberDetails();
+    genAuthentication(member);
 
-        BudgetUpdateRequest req = new BudgetUpdateRequest(
-                null,
-                true,
-                BigDecimal.valueOf(1393.7)
-        );
+    BudgetUpdateRequest req = new BudgetUpdateRequest(
+        null,
+        true,
+        BigDecimal.valueOf(1393.7)
+    );
 
-        when(budgetService.updateByTeamId(teamId, member.getId(), req))
-                .thenThrow(new CustomLogicException(ExceptionCode.BAD_REQUEST));
+    when(budgetService.updateByTeamId(teamId, member.getId(), req))
+        .thenThrow(new CustomLogicException(ExceptionCode.BAD_REQUEST));
 
-        mockMvc.perform(
-                        patch("/api/teams/{teamId}/budgets", teamId)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(req))
-                )
-                .andExpect(status().isBadRequest())
-                .andDo(print());
+    mockMvc.perform(
+            patch("/api/teams/{teamId}/budgets", teamId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(req))
+        )
+        .andExpect(status().isBadRequest())
+        .andDo(print());
 
-    }
+  }
 
-    @Test
-    @DisplayName("PATCH /api/teams/{teamId}/budgets/add를 요청해 예산 추가에 성공하면 200 OK를 응답한다")
-    void add_should_return_200() throws Exception {
+  @Test
+  @DisplayName("PATCH /api/teams/{teamId}/budgets/add를 요청해 예산 추가에 성공하면 200 OK를 응답한다")
+  void add_should_return_200() throws Exception {
 
-        Long teamId = 1L;
+    Long teamId = 1L;
 
-        MemberDetails member = genMemberDetails();
-        genAuthentication(member);
+    MemberDetails member = genMemberDetails();
+    genAuthentication(member);
 
-        BudgetAddRequest req = genBudgetAddReq();
-        BudgetUpdateResponse resp = genBudgetUpdateRespAfterAdd();
+    BudgetAddRequest req = genBudgetAddReq();
+    BudgetUpdateResponse resp = genBudgetUpdateRespAfterAdd();
 
-        when(budgetService.addBudgetByTeamId(teamId, member.getId(), req)).thenReturn(resp);
+    when(budgetService.addBudgetByTeamId(teamId, member.getId(), req)).thenReturn(resp);
 
-        mockMvc.perform(
-                        patch("/api/teams/{teamId}/budgets/add", teamId)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(req))
-                )
-                .andExpect(status().isOk())
-                .andDo(print());
+    mockMvc.perform(
+            patch("/api/teams/{teamId}/budgets/add", teamId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(req))
+        )
+        .andExpect(status().isOk())
+        .andDo(print());
 
-        verify(budgetValidator, times(1)).validateRequest(req);
-    }
+    verify(budgetValidator, times(1)).validateRequest(req);
+  }
 
-    @Test
-    @DisplayName("additionalBudget을 null로 요청한 경우 예산 수정에 실패하고 400 BAD REQUEST를 응답한다")
-    void add_should_return_400() throws Exception {
+  @Test
+  @DisplayName("additionalBudget을 null로 요청한 경우 예산 수정에 실패하고 400 BAD REQUEST를 응답한다")
+  void add_should_return_400() throws Exception {
 
-        Long teamId = 1L;
+    Long teamId = 1L;
 
-        MemberDetails member = genMemberDetails();
-        genAuthentication(member);
+    MemberDetails member = genMemberDetails();
+    genAuthentication(member);
 
-        BudgetAddRequest req = new BudgetAddRequest(
-                null,
-                false,
-                null
-        );
+    BudgetAddRequest req = new BudgetAddRequest(
+        null,
+        false,
+        null
+    );
 
-        when(budgetService.addBudgetByTeamId(teamId, member.getId(), req))
-                .thenThrow(new CustomLogicException(ExceptionCode.BAD_REQUEST));
+    when(budgetService.addBudgetByTeamId(teamId, member.getId(), req))
+        .thenThrow(new CustomLogicException(ExceptionCode.BAD_REQUEST));
 
-        mockMvc.perform(
-                        patch("/api/teams/{teamId}/budgets/add", teamId)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(req))
-                )
-                .andExpect(status().isBadRequest())
-                .andDo(print());
+    mockMvc.perform(
+            patch("/api/teams/{teamId}/budgets/add", teamId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(req))
+        )
+        .andExpect(status().isBadRequest())
+        .andDo(print());
 
-    }
+  }
 
 }
